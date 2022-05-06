2db9aeaa424cc971e1a02c50a62ea05099809e68
B.G. Manasa <BG.Manasa@BRILLIO.COM>
2022-05-06 17:24:29 +0530

    final commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("./db");
 const app = express();
-// const expEjsLayouts = require("express-ejs-layouts");
+const expEjsLayouts = require("express-ejs-layouts");
 const PatientRoute = require("./router/patient.routes");
 const VaccineRoute= require("./router/vaccine.routes");
 const cors = require("cors");
@@ -9,6 +9,11 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+app.use(expEjsLayouts);
+app.set("layout", "./layouts/layout");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -22,8 +27,3 @@ const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log("Server started at PORT : ", PORT));
 
 
-// app.use(express.static(__dirname + "/public"));
-// app.use(expEjsLayouts);
-// app.set("layout", "./layouts/layout");
-// app.set("view engine", "ejs");
-// app.use(express.static(__dirname + "/public"));
